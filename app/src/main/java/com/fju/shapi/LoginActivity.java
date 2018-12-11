e6fcf40bd0e8467b55745b5afe1a9e031fcc70bf
Mavis <angel253698@gmail.com>
2018-12-11 15:56:35 +0800

    LoginActivity 2

--- app/src/main/java/com/fju/shapi/LoginActivity.java
+++ app/src/main/java/com/fju/shapi/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.fju.shapi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,31 +17,52 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edPassword;
     private String email;
     private String password;
+    private TextView edAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         findView();
-
+        String email  = getSharedPreferences("login" , MODE_PRIVATE).getString("Email", " ");
+        edEmail.setText(email);
     }
 
     private void findView() {
         edEmail = findViewById(R.id.ed_email);
         edPassword = findViewById(R.id.ed_password);
+        edAlert = findViewById(R.id.ed_alert);
+
+
+    }
+
+    public void cancel(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view){
+        findView();
         email = edEmail.getText().toString();
         password = edPassword.getText().toString();
-        if("12345@gmail.com".equals(email) && "12345".equals(password)){
+
+        if("12345".equals(email) && "12345".equals(password)){
             getSharedPreferences("login",MODE_PRIVATE)
                     .edit()
                     .putString("Email",email)
                     .apply();
+            setResult(RESULT_OK);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
         }else {
-            Toast.makeText(this, "xxx", Toast.LENGTH_LONG).show();
+//            edAlert.setText("xxx");
+            Toast xToast = Toast.makeText(this,"登入失敗xxx",Toast.LENGTH_LONG);
+            xToast.setGravity(Gravity.TOP|Gravity.LEFT, 10, 850);
+            xToast.show();
+           // Toast.makeText(this, "xxx", Toast.LENGTH_LONG).show();
             edEmail.setText(" ");
             edPassword.setText(" ");
         }
     }
-
 
 }
--- app/src/main/java/com/fju/shapi/MainActivity.java
+++ app/src/main/java/com/fju/shapi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fju.shapi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,10 +8,17 @@ import com.fju.shapi.data.DummyData;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final int RC_LOGIN = 7800;
+    boolean login = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (!login) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent, RC_LOGIN);
+        }
     }
 }
