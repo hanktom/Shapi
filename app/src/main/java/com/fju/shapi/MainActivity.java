a07c669f2dcf223dd128d3623d7bdd4d29e9ee71
Hank Tom <hanktom@gmail.com>
2018-12-11 15:01:03 +0800

    Product, News and DummyData for Products

--- app/src/main/java/com/fju/shapi/MainActivity.java
+++ app/src/main/java/com/fju/shapi/MainActivity.java
@@ -3,11 +3,14 @@ package com.fju.shapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.fju.shapi.data.DummyData;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/fju/shapi/data/Product.java
@@ -0,0 +1,57 @@
+package com.fju.shapi.data;
+
+public class Product {
+    int id;
+    String name;
+    String info;
+    int price;
+    int stock;
+
+    public Product(int id, String name, String info, int price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.info = info;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
