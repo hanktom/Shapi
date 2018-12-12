90742427b4fce3534305ee815003871e7703ba6e
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2018-12-12 21:13:11 +0800

    getSharePreference-PD part

--- app/src/main/java/com/fju/shapi/PersonalDataActivity.java
+++ app/src/main/java/com/fju/shapi/PersonalDataActivity.java
@@ -10,23 +10,52 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.fju.shapi.data.DummyData;
+import com.fju.shapi.data.PersonalData;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonalDataActivity extends AppCompatActivity {
-    EditText edValue;
-    TextView tvName;
-    List<String> data = new ArrayList<>();
+    EditText edName;
+    EditText edGender;
+    EditText edBirth;
+    EditText edTel;
+    EditText edAddress;
+    PersonalData personalData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
+        edName.setText(personalData.getName());
+        edGender.setText(personalData.getGender());
+        edBirth.setText(personalData.getBirth());
+        edTel.setText(personalData.getTel());
+        edAddress.setText(personalData.getAdress());
      }
     public void findViews() {
+        edName = findViewById(R.id.ed_PD_name);
+        edGender = findViewById(R.id.ed_PD_gender);
+        edBirth = findViewById(R.id.ed_PD_birth);
+        edTel = findViewById(R.id.ed_PD_tel);
+        edAddress = findViewById(R.id.ed_PD_address);
+    }
+    public void save(View view){
+        findViews();
+        String name = edName.getText().toString();
+        String gender = edGender.getText().toString();
+        String birth = edBirth.getText().toString();
+        String tel = edTel.getText().toString();
+        String address = edAddress.getText().toString();
+        getSharedPreferences("userPersonalData",MODE_PRIVATE)
+                .edit()
+                .putString("USERNAME",name)
+                .putString("USERGENDER",gender)
+                .putString("USERBIRTH",birth)
+                .putString("USERTEL",tel)
+                .putString("USERADDRESS",address)
+                .commit();
 
     }
-
 
 }
--- app/src/main/java/com/fju/shapi/data/PersonalData.java
+++ app/src/main/java/com/fju/shapi/data/PersonalData.java
@@ -1,5 +1,9 @@
 package com.fju.shapi.data;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,59 +13,59 @@ String birth;
 String gender;
 String adress;
 String tel;
-
-    public PersonalData() {
-
+SharedPreferences settings;
+Context context;
+    public PersonalData(Context context) {
+        this.context = context;
+        settings = context.getSharedPreferences("userPersonalData",Context.MODE_PRIVATE);
     }
 
     public String getName() {
-        return name;
+        return settings.getString("USERNAME",null);
     }
 
     public void setName(String name) {
+        settings.edit().putString("USERNAME",name).apply();
         this.name = name;
     }
 
     public String getBirth() {
-        return birth;
+        return settings.getString("USERBIRTH",null);
     }
 
     public void setBirth(String birth) {
+        settings.edit().putString("USERBIRTH",birth).apply();
         this.birth = birth;
     }
 
 
     public String getGender() {
-        return gender;
+        return settings.getString("USERGENDER",null);
     }
 
     public void setGender(String gender) {
+        settings.edit().putString("USERGENDER",gender).apply();
         this.gender = gender;
     }
 
     public String getAdress() {
-        return adress;
+        return settings.getString("USERADDRESS",null);
     }
 
     public void setAdress(String adress) {
+        settings.edit().putString("USERADDRESS",adress).apply();
         this.adress = adress;
     }
 
     public String getTel() {
-        return tel;
+        return settings.getString("USERTEL",null);
     }
 
     public void setTel(String tel) {
+        settings.edit().putString("USERTEL",tel).apply();
         this.tel = tel;
     }
 
-    public PersonalData(String name, String birth, String gender, String adress, String tel) {
 
-        this.name = name;
-        this.birth = birth;
-        this.gender = gender;
-        this.adress = adress;
-        this.tel = tel;
-    }
 
 }
